83901ce1201c8678bd39f4f7203a7ccfefa4e837
YassineOularbi <yassineoularbi4@gmail.com>
2024-02-21 16:22:20 +0100

    Ajout de attribues de la class City

--- src/City.java
+++ src/City.java
@@ -1,3 +1,7 @@
 public class City {
-
+    private Integer cityId;
+    private String cityName;
+    private Integer currentTemperature;
+    private Integer currentHumidity;
+    private Integer currentWindSpeed;
 }
