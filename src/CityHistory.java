82d1bd6c6a65bad5a7a774753e613623a2607517
YassineOularbi <yassineoularbi4@gmail.com>
2024-02-21 16:26:41 +0100

    Ajout de constructeur Classe CityHistory

--- src/CityHistory.java
+++ src/CityHistory.java
@@ -7,5 +7,12 @@ public class CityHistory {
     private Integer historicalTemperature;
     private Integer historicalHumidity;
     private Integer historicalWindSpeed;
-
+    public CityHistory(Integer historicalDataId, Integer cityId, LocalDate eventDate, Integer historicalTemperature, Integer historicalHumidity, Integer historicalWindSpeed) {
+        this.historicalDataId = historicalDataId;
+        this.cityId = cityId;
+        this.eventDate = eventDate;
+        this.historicalTemperature = historicalTemperature;
+        this.historicalHumidity = historicalHumidity;
+        this.historicalWindSpeed = historicalWindSpeed;
+    }
 }
