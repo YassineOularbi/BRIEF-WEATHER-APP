2874e925c8ef0de76401bcf1b601b1cc99cffd19
YassineOularbi <yassineoularbi4@gmail.com>
2024-02-21 16:23:17 +0100

    Ajout de attribues de la class CityHistory

--- src/CityHistory.java
+++ src/CityHistory.java
@@ -1,2 +1,10 @@
+import java.time.LocalDate;
+
 public class CityHistory {
+    private Integer historicalDataId;
+    private Integer cityId;
+    private LocalDate eventDate;
+    private Integer historicalTemperature;
+    private Integer historicalHumidity;
+    private Integer historicalWindSpeed;
 }
