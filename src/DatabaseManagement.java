1d22a3dce5e069cf72074b207c4a9805fc4ef020
YassineOularbi <yassineoularbi4@gmail.com>
2024-02-22 12:02:12 +0100

    Ajout de la méthode de connection avec la base de donnée

--- src/DatabaseManagement.java
+++ src/DatabaseManagement.java
@@ -1,2 +1,14 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class DatabaseManagement {
+    private static final String url = "jdbc:mysql://localhost:3306/data_Weather";
+    private static final String username = "root";
+    private static final String password = "VANWOLFDEV123@";
+
+    private static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(url, username, password);
+    }
+
 }
